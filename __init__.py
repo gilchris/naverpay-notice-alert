71e972ed51a8b744505f9eac1571369ba93a8e74
Changgun Lee <lazysense@gmail.com>
2023-10-30 00:41:37 +0900

    새 공지사항이 없을 때 에러 처리 추가

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ if os.path.exists(filePath):
     with open(filePath, 'r') as f:
         oldArticles = json.load(f)
 
-        # 예전 데이터와 비교해서 동일하면 그냥 종료. 그런데 귀찮기도 하고 맨 위가 다르면 결국 다른 거니까 최상단에 하나만 비표해본다.
+        # 예전 데이터와 비교해서 동일하면 그냥 종료. 그런데 귀찮기도 하고 맨 위가 다르면 결국 다른 거니까 최상단에 하나만 비교해본다.
         if len(oldArticles) > 0 and oldArticles[0]["title"] == currentArticles[0]["title"] and oldArticles[0]["url"] == currentArticles[0]["url"]:
             exit()
 
@@ -32,7 +32,8 @@ for currentArticle in currentArticles:
     if isStop:
         break
     newArticles.append(currentArticle)
-
+if len(newArticles) == 0:
+    exit()
 
 content = {
     'content': '새로운 네이버페이 공지사항이 등록되었습니다.',
